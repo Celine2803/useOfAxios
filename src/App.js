0edb6f6d6400d6dc3951ae8a0baf97cfad628ff6
Celine Kerubo <celinekerubo28@gmail.com>
2023-02-14 21:13:36 +0300

    an axios commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
+import { useEffect,useState } from 'react';
 import './App.css';
+import Axios from 'axios';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+
 
 function App() {
+  const[data,setDate]=useState([])
+  useEffect(()=>{
+    Axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res=>{
+      console.log(res.data)
+    setDate(res.data)
+  }).catch(err=>console.log(err))
+  },[])
+
+  const arr=data.map((data,index)=>{
+    return(
+      <tbody><tr>
+      <td>{data.id}<button><AccessAlarmIcon/></button></td>
+      <td>{data.title}</td>
+      <td>{data.body}</td>
+    </tr>
+    </tbody>
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <table>
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th>TITLE</th>
+          <th>BODY</th>
+        </tr>
+        </thead>
+        {arr}
+        
+      </table>
     </div>
   );
 }
